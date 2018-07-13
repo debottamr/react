b490d23223c9d63ad9e2c019fbdf7ca270fb21ba
debottam.r@gmail.com <root@localhost.localdomain>
2018-07-13 01:30:15 -0400

    Functional

---
+++ FunctionalComponent/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes'
+import Main from './components/MainComponent'
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
