3af50e9b47ce29e5877d7494fdd6a4cc3e5dfa45
debottam.r@gmail.com <root@localhost.localdomain>
2018-07-13 07:40:52 -0400

    Single Final Commit

--- Single/src/components/MainComponent.js
+++ Single/src/components/MainComponent.js
@@ -6,15 +6,20 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -24,9 +29,12 @@ class Main extends Component {
 
   render() {
 
-    const HomePage = () => {
+     const HomePage = () => {
       return(
           <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -37,6 +45,8 @@ class Main extends Component {
            <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
+
               <Redirect to="/home" />
           </Switch>
           <Footer />
