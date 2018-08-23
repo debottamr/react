3ec77a08d0641cc3f266459ee8e64fce66935174
Debottamr <debottam.r@gmail.com>
2018-08-23 07:54:52 -0700

    New Form Changes

--- reduxasync/src/components/course/CoursesPage.js
+++ reduxasync/src/components/course/CoursesPage.js
@@ -1,7 +1,9 @@
 import React, {PropTypes} from 'react';
-import * as courseActions from '../../actions/courseActions';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -12,21 +14,25 @@ class CoursesPage extends React.Component {
     return <div key={index}>{course.title}</div>;
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
     const {courses} = this.props;
 
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+           <CourseList courses={courses}/>
       </div>
     );
   }
 }
 
 CoursesPage.propTypes = {
-  actions: PropTypes.object.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
---
+++ reduxasync/src/components/course/ManageCoursePage.js
@@ -0,0 +1,49 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
+
+export class ManageCoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+   
+    this.state = {
+        course: Object.assign({},this.props.course),
+        errors:{}
+    };
+  }
+  render() {
+    return (
+      <div>
+            <h1>Manage Course</h1>
+            <CourseForm 
+            course = {this.state.course}
+            errors = {this.state.errors}
+            allAuthors = {[]}
+             />
+      </div>
+    );
+  }
+}
+
+ManageCoursePage.PropTypes = {
+  
+};
+
+
+function mapStateToProps(state, ownProps) {
+ 
+  let course = {id: '', watchHref:'', title: '', length: '', category: ''};
+  return {
+    course: course
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
