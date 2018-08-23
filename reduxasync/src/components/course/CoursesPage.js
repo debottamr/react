496a066b651732caa06243dff9bdd3101eea886a
Debottamr <debottam.r@gmail.com>
2018-08-23 07:16:57 -0700

    async

---
+++ reduxasync/src/components/course/CoursesPage.js
@@ -0,0 +1,44 @@
+import React, {PropTypes} from 'react';
+import * as courseActions from '../../actions/courseActions';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  render() {
+    const {courses} = this.props;
+
+    return (
+      <div>
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
